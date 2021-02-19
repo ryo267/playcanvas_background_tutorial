1d55dbfd66a21a65deed3caf3e9386782dabe950
ryo267 <bigbox267@gmail.com>
2021-02-19 15:34:50 +0900

    first-commit

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1094625.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 42907163, 42911928, 42912073, 42912138 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/42912014/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/42912015/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/42912013/1/ammo.js', 'preload' : true},
+];
